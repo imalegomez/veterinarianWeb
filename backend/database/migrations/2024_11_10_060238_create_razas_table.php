8f4f18f4ebc0c302bb57d044b744642419f38c61
imalegomez <imalegomez2004@gmail.com>
2024-11-11 19:49:24 -0300

    Migraciones && BDD

--- backend/database/migrations/2024_11_10_060238_create_razas_table.php
+++ backend/database/migrations/2024_11_10_060238_create_razas_table.php
@@ -4,24 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateRazasTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('razas', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('especie_id')->constrained()->onDelete('cascade');
+            $table->string('nombre');
+            $table->text('caracteristicas')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('razas');
     }
-};
+}
